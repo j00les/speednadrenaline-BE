998ff1d943f75da62b674800780f0e43e55cfbf9
Nabiel Alif <nabiel.alif01@gmail.com>
2025-01-22 11:03:03 +0700

    feat: add middleware

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.use(
     credentials: true
   })
 );
+app.use((req, res, next) => {
+  console.log(` middleware baru dipasang: ${req.method} ${req.url}`);
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
