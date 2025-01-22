1346d74cbc2cb88748f1d5e571b86d9ec4406f6a
Nabiel Alif <nabiel.alif01@gmail.com>
2025-01-22 11:29:00 +0700

    feat: allow origin

--- app.js
+++ app.js
@@ -13,17 +13,13 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING);
 
 app.use(
   cors({
-    origin: ['https://speednadrenaline.com'], // Your frontend domain
+    origin: '*', // Your frontend domain
     credentials: true
   })
 );
-app.use((req, res, next) => {
-  console.log(` middleware baru dipasang: ${req.method} ${req.url}`);
-  next();
-});
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/api-save-overall', async (req, res) => {
   try {
