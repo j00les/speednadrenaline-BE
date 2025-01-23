7f96e84073bde10cc50521b44b2bdebdac0ead78
Nabiel Alif <nabiel.alif01@gmail.com>
2025-01-23 15:30:46 +0700

    feat: add cartype

--- models/bestTime.js
+++ models/bestTime.js
@@ -3,17 +3,18 @@ const mongoose = require('mongoose');
 const DriverSchema = new mongoose.Schema({
   name: { type: String, required: true },
   carName: { type: String, required: true },
+  carType: { type: String, required: true },
   lapTime: { type: String, required: true },
   gapToFirst: { type: String, required: true }
 });
 
 const BestTimeSchema = new mongoose.Schema(
   {
-    drivers: [DriverSchema], // Array of driver objects
-    savedAt: { type: Date, default: Date.now } // Timestamp
+    drivers: [DriverSchema],
+    savedAt: { type: Date, default: Date.now }
   },
 
-  { collection: 'best_times' } // Explicitly set the collection name
+  { collection: 'best_times' }
 );
 
 const BestTime = mongoose.model('BestTime', BestTimeSchema);
